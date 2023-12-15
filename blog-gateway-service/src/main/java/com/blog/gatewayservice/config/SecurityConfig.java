2e96714d683521a115985e8e81db1cb085c0f47d
Le Khac Anh Dai <khacdai0801@gmail.com>
2023-12-15 16:17:16 +0700

    impl oauth2 with resource server

---
+++ blog-gateway-service/src/main/java/com/blog/gatewayservice/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.blog.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author dai.le-anh
+ * @since 12/15/2023
+ */
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authz -> authz
+                .anyRequest()
+                .fullyAuthenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer
+                        .withDefaults()));
+        return http.build();
+    }
+}
